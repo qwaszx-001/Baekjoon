8c38051f6eb57f604e87e212dc52e2fc9b6aa16f
Jhin <105303355+qwaszx-001@users.noreply.github.com>
2025-07-15 13:27:22 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>  //n번째 줄에 n개의 별(1<=n<=100)
+int main(){
+    int n;
+    scanf("%d", &n); 
+    for(int i=1; i<=n; i++){
+        for(int k=1; k<=n-i; k++){
+            printf(" ");
+        }
+        for(int k=1; k<=i; k++){
+            printf("*");
+            }
+        printf("\n");
+    }return 0;
+}
